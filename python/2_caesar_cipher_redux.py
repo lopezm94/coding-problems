482254581664737e4181d928229603ccc41dc49a
Juan López <lopezm@Juans-MacBook-Air.local>
2023-12-11 23:46:34 +0100

    Add caesar cipher redux solution

---
+++ python/2_caesar_cipher_redux.py
@@ -0,0 +1,24 @@
+import argparse
+
+# shift_chr(char: str) -> int
+# Example: shift_chr('a', 1) -> 98
+def shift_chr(char: str, shift: int) -> int:
+    return chr(97 + (ord(char) - 97 + shift) % 26)
+
+# shift_letters(string: str, shift: int) -> str
+# Expects a string and an integer. Returns the string with each character shifted by the integer.
+# Example: shift_letters('abc', 1) -> 'bcd'
+def shift_letters(string: str, shift: int) -> str:
+    result = ""
+    for char in string:
+        result += shift_chr(char, shift)
+    return result
+
+# Example: python 2_caesar_cipher_redux.py 'abc' 1 -> 'bcd'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Shift letters in a string by a given number.')
+    parser.add_argument('string', type=str, help='The string to shift')
+    parser.add_argument('shift', type=int, help='The number of positions to shift')
+    args = parser.parse_args()
+    result = shift_letters(args.string, args.shift)
+    print(result)
--- python/README.md
+++ python/README.md
@@ -7,8 +7,8 @@ For type verifications execute `mypy <file_path>`
 ### 1. Sum of a Range of Integers
 **Objective:** Create the `add_it_up()` function to sum integers from 0 to the input. Non-integer inputs should return 0.
 
-### 2. Caesar Cipher
-**Objective:** Implement a function for the Caesar cipher that shifts letters by a given number, maintaining punctuation, whitespace, and case sensitivity.
-
-### 3. Caesar Cipher Redux
-**Objective:** Redo the Caesar cipher without using `.translate()`, focusing on alternate methods and problem-solving skills.
+### 2. Caesar Cipher Redux
+**Objective:** Implement a function for the Caesar cipher that shifts letters by a given number, maintaining punctuation, whitespace, and case sensitivity. You should not use the `.translate()` function.
+**Example:**
+- Input: `"abc", 2`
+- Output: `"cde"` (each letter shifted by 2)
