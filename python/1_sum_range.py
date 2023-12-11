5a8314d78c94e3acfc67327ecc4e21c7232f661b
Juan López <lopezm@Juans-MacBook-Air.local>
2023-12-11 23:17:17 +0100

    Add add_it_up problem solution

---
+++ python/1_sum_range.py
@@ -0,0 +1,18 @@
+import argparse
+
+# add_it_up(n: int) -> int
+# Expects a single integer argument and returns the sum of all integers from 0 to that number.
+# If the argument is not an integer, or is less than zero, return 0.
+# Example: add_it_up(5) -> 15
+def add_it_up(n: int) -> int:
+    if not isinstance(n, int) or n < 0:
+        return 0
+    return sum(range(n + 1))
+
+# Expects a single integer argument and returns the sum of all integers from 0 to that number.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Sum up integers from 0 to a given number.')
+    parser.add_argument('number', type=int, help='The number to sum up to')
+    args = parser.parse_args()
+    result = add_it_up(args.number)
+    print(result)
---
+++ python/README.md
@@ -0,0 +1,14 @@
+# Python Practice Problems
+
+For type verifications execute `mypy <file_path>`
+
+## Problem Summaries
+
+### 1. Sum of a Range of Integers
+**Objective:** Create the `add_it_up()` function to sum integers from 0 to the input. Non-integer inputs should return 0.
+
+### 2. Caesar Cipher
+**Objective:** Implement a function for the Caesar cipher that shifts letters by a given number, maintaining punctuation, whitespace, and case sensitivity.
+
+### 3. Caesar Cipher Redux
+**Objective:** Redo the Caesar cipher without using `.translate()`, focusing on alternate methods and problem-solving skills.
